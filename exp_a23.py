66cc4af63f7a928296826249ed860ba2ca6bd285
Almudena Carrera <acv@zurich.ibm.com>
2019-11-19 07:18:17 +0100

    computing analytical and vector (there are some errors)

--- exp_a23.py
+++ exp_a23.py
@@ -24,9 +24,6 @@ class A23Factory(CircuitFactory):
             return max(1, self._num_target_qubits - 1)
 
     def build(self, qc, q, q_ancillas=None, params=1):
-        # Gates for A2
-        qc.u3(-2*params, -np.pi/2, np.pi/2, q[0])
-
         # Gates for A3
         for i in range(0, self.num_target_qubits-1):
             q_controls = []
@@ -52,74 +49,11 @@ class A23Factory(CircuitFactory):
             qc.x(q[i])
             qc.cx(q[i], q[i+1])
 
-        # Below method linear in nb - too slow for few qubits
-        # # Gates for A2
-        # qc.u3(-2*params, -np.pi/2, np.pi/2, q[0])
-
-        # # Gates for A3
-        # # Shit by +1 all basis states except 0 and nb-1. Last ancilla qubit contains flag for 0 and -1
-        # q_controls=[]
-        # qa = []
-        # for i in range(0, q.size-1):
-        #     qa.append(q_ancillas[i])
-        # qc.x(q_ancillas[q.size-1])
-        # for i in range(0, q.size):
-        #     q_controls.append(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-        # qc.cx(q_ancillas[q.size-1], q[0])
-        # for i in range(1, q.size-1):
-        #     qc.ccx(q[i], q_ancillas[i-1],  q_ancillas[i])
-        #     qc.cx(q_ancillas[i-1], q[i])
-        # qc.cx(q_ancillas[q.size-2],q[q.size-1])
-        # # Uncompute ancilla
-        # for i in reversed(range(1, q.size-1)):
-        #     qc.x(q[i])
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        #     qc.x(q[i])
-        # qc.x(q[0])
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-        # qc.x(q[0])
-
-        # # Ry
-        # qc.cu3(-2*params, -np.pi/2, np.pi/2, q_ancillas[q.size-1], q[0])
-
-        # # Shift by -1 all basis states
-        # qc.cx(q_ancillas[q.size-1], q[0])
-        # qc.ccx(q[0],q_ancillas[q.size-1], q_ancillas[0])
-        # for i in range(1, q.size-1):
-        #     qc.x(q[i])
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        #     qc.x(q[i])
-        #     qc.cx(q_ancillas[i-1], q[i])
-        # qc.cx(q_ancillas[q.size-2],q[q.size-1])
-        # # Uncompute ancilla
-        # for i in reversed(range(1, q.size-1)):
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-
-        # # Uncompute last ancilla
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-
 
     def build_inverse(self, qc, q, q_ancillas=None, params=1):
         self.build(qc, q, q_ancillas, -params)
 
     def build_controlled(self, qc, q, q_control, q_ancillas=None, params=1):
-        # Gates for A2 with t
-        qc.cu3(-2*params, -np.pi/2, np.pi/2, q_control, q[0])
-
         # Gates for A3
         for i in range(0, self.num_target_qubits-1):
             q_controls = []
@@ -146,69 +80,6 @@ class A23Factory(CircuitFactory):
                 qc.cx(q[i], q[j])
             qc.x(q[i])
             qc.cx(q[i], q[i+1])
-
-        # Below method linear in nb - too slow for few qubits
-        # # Gates for A2
-        # qc.cu3(-2*params, -np.pi/2, np.pi/2, q_control, q[0])
-
-        # # Gates for A3
-        # # Shit by +1 all basis states except 0 and nb-1. Last ancilla qubit contains flag for 0 and -1
-        # qa = []
-        # for i in range(0, q.size-1):
-        #     qa.append(q_ancillas[i])
-        # q_controls=[q_control]
-        # # Only simulate if q_control is 1
-        # qc.cx(q_control, q_ancillas[q.size-1])
-
-        # for i in range(0, q.size):
-        #     q_controls.append(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-        # qc.cx(q_ancillas[q.size-1], q[0])
-        # for i in range(1, q.size-1):
-        #     qc.ccx(q[i], q_ancillas[i-1],  q_ancillas[i])
-        #     qc.cx(q_ancillas[i-1], q[i])
-        # qc.cx(q_ancillas[q.size-2],q[q.size-1])
-        # # Uncompute ancilla
-        # for i in reversed(range(1, q.size-1)):
-        #     qc.x(q[i])
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        #     qc.x(q[i])
-        # qc.x(q[0])
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-        # qc.x(q[0])
-
-        # # Ry
-        # qc.cu3(-2*params, -np.pi/2, np.pi/2, q_ancillas[q.size-1], q[0])
-
-        # # Shift by -1 all basis states
-        # qc.cx(q_ancillas[q.size-1], q[0])
-        # qc.ccx(q[0],q_ancillas[q.size-1], q_ancillas[0])
-        # for i in range(1, q.size-1):
-        #     qc.x(q[i])
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        #     qc.x(q[i])
-        #     qc.cx(q_ancillas[i-1], q[i])
-        # qc.cx(q_ancillas[q.size-2],q[q.size-1])
-        # # Uncompute ancilla
-        # for i in reversed(range(1, q.size-1)):
-        #     qc.ccx(q[i], q_ancillas[i-1], q_ancillas[i])
-        # qc.ccx(q[0], q_ancillas[q.size-1], q_ancillas[0])
-
-        # # Uncompute last ancilla
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-        # multi_toffoli_q(qc, q_controls, q_ancillas[q.size-1], qa)
-        # for i in range(0,q.size):
-        #     qc.x(q[i])
-        # qc.cx(q_control, q_ancillas[q.size-1])
 
     def build_controlled_inverse(self, qc, q, q_control, q_ancillas=None, params=1):
         self.build_controlled(qc, q, q_control, q_ancillas, -params)
